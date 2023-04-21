8c2b2e0d10fe0d5c666b32bf23e7395450054812
edederhlf <ederhlf@gmail.com>
2023-04-21 10:05:31 -0300

    auto_layout_aspect_ratio_constraints_implement

--- Project_2/Project_2/View.swift
+++ Project_2/Project_2/View.swift
@@ -3,8 +3,8 @@ import Foundation
 import UIKit
 
 protocol ViewDelegate: AnyObject {
-  func tapOnFlag(btnTag: Int)
-  func nextChallenge()
+    func tapOnFlag(btnTag: Int)
+    func nextChallenge()
 }
 
 class View: UIView {
@@ -18,19 +18,34 @@ class View: UIView {
     var delegate: ViewDelegate?
     var flagsBtns: [UIButton] = [UIButton]()
     
+    var heightFirstFlag = NSLayoutConstraint()
+    var widthFirstFlag = NSLayoutConstraint()
+    
+    var heightSecondFlag = NSLayoutConstraint()
+    var widthSecondFlag = NSLayoutConstraint()
+    
+    var heightThirdFlag = NSLayoutConstraint()
+    var widthThirdFlag = NSLayoutConstraint()
+    
+    var heightNextBtn = NSLayoutConstraint()
+    var widthNextBtn = NSLayoutConstraint()
+    
     var flagNames: [String] = [] {
         didSet {
             configureFirstFlagBtnLayout()
             configureSecondFlagBtnLayout()
             configureThirdFlagBtnLayout()
+            configureNextBtnLayout()
         }
     }
     
     // MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configureNextBtnLayout()
         flagsBtns += [firstFlagBtn, secondFlagBtn, thirdFlagBtn]
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(refreshConstraints), name: UIDevice.orientationDidChangeNotification, object: nil)
+        refreshConstraints()
     }
     
     override func layoutSubviews() {
@@ -56,7 +71,55 @@ class View: UIView {
         thirdFlagBtn.isEnabled = isEnable
     }
     
-    
+    @objc func refreshConstraints() {
+        NSLayoutConstraint.deactivate(
+            [
+               widthFirstFlag,
+               heightFirstFlag,
+               heightSecondFlag,
+               widthSecondFlag,
+               heightThirdFlag,
+               widthThirdFlag,
+                heightNextBtn,
+                widthNextBtn
+            ]
+        )
+        
+        if frame.width > frame.height {
+            widthFirstFlag = firstFlagBtn.widthAnchor.constraint(equalToConstant: 100)
+            heightFirstFlag = firstFlagBtn.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 1/5)
+            widthSecondFlag = secondFlagBtn.widthAnchor.constraint(equalToConstant: 100)
+            heightSecondFlag = secondFlagBtn.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 1/5)
+            widthThirdFlag = thirdFlagBtn.widthAnchor.constraint(equalToConstant: 100)
+            heightThirdFlag = thirdFlagBtn.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 1/5)
+            heightNextBtn = nextBtn.widthAnchor.constraint(equalToConstant: 50)
+            widthNextBtn = nextBtn.heightAnchor.constraint(equalToConstant: 50)
+            
+        }
+        else {
+            widthFirstFlag = firstFlagBtn.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 1/2)
+            heightFirstFlag = firstFlagBtn.heightAnchor.constraint(equalToConstant: 100)
+            widthSecondFlag = secondFlagBtn.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 1/2)
+            heightSecondFlag = secondFlagBtn.heightAnchor.constraint(equalToConstant: 100)
+            widthThirdFlag = thirdFlagBtn.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 1/2)
+            heightThirdFlag = thirdFlagBtn.heightAnchor.constraint(equalToConstant: 100)
+            heightNextBtn = nextBtn.widthAnchor.constraint(equalToConstant: 100)
+            widthNextBtn = nextBtn.heightAnchor.constraint(equalToConstant: 100)
+        }
+        
+        NSLayoutConstraint.activate(
+            [
+                widthFirstFlag,
+                heightFirstFlag,
+                heightSecondFlag,
+                widthSecondFlag,
+                heightThirdFlag,
+                widthThirdFlag,
+                heightNextBtn,
+                widthNextBtn
+            ]
+        )
+    }
 }
 
 // MARK: - First BTN
@@ -73,14 +136,16 @@ extension View {
         firstFlagBtn.setImage(UIImage(named: flagNames[0]), for: .normal)
         firstFlagBtn.translatesAutoresizingMaskIntoConstraints = false
         firstFlagBtn.addTarget(self, action: #selector(tapOnFlag), for: .touchUpInside)
+        firstFlagBtn.contentMode = .scaleAspectFit
+        firstFlagBtn.layer.masksToBounds = true
+        firstFlagBtn.clipsToBounds = true
     }
     
     func configureFirstFlagBtnConstraints() {
-        NSLayoutConstraint.activate([
-            firstFlagBtn.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
-            firstFlagBtn.leftAnchor.constraint(equalTo: leftAnchor, constant: 100),
-            firstFlagBtn.rightAnchor.constraint(equalTo: rightAnchor, constant: -100)
-        ])
+        firstFlagBtn.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        firstFlagBtn.topAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        widthFirstFlag.isActive = true
+        heightFirstFlag.isActive = true
     }
 }
 
@@ -98,14 +163,16 @@ extension View {
         secondFlagBtn.setImage(UIImage(named: flagNames[1]), for: .normal)
         secondFlagBtn.translatesAutoresizingMaskIntoConstraints = false
         secondFlagBtn.addTarget(self, action: #selector(tapOnFlag), for: .touchUpInside)
+        secondFlagBtn.contentMode = .scaleAspectFit
+        secondFlagBtn.layer.masksToBounds = true
+        secondFlagBtn.clipsToBounds = true
     }
     
     func configureSecondFlagBtnConstraints() {
-        NSLayoutConstraint.activate([
-            secondFlagBtn.topAnchor.constraint(equalTo: firstFlagBtn.bottomAnchor, constant: 20),
-            secondFlagBtn.leftAnchor.constraint(equalTo: leftAnchor, constant: 100),
-            secondFlagBtn.rightAnchor.constraint(equalTo: rightAnchor, constant: -100)
-        ])
+        secondFlagBtn.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        secondFlagBtn.topAnchor.constraint(greaterThanOrEqualTo: firstFlagBtn.bottomAnchor, constant: 10).isActive = true
+        widthSecondFlag.isActive = true
+        heightSecondFlag.isActive = true
     }
 }
 
@@ -113,25 +180,26 @@ extension View {
 extension View {
     func configureThirdFlagBtnLayout() {
         addSubview(thirdFlagBtn)
-
+        
         configureThirdFlagBtnSpecs()
         configureThirdFlagBtnConstraints()
     }
-
+    
     func configureThirdFlagBtnSpecs() {
         thirdFlagBtn.tag = 2
         thirdFlagBtn.setImage(UIImage(named: flagNames[2]), for: .normal)
         thirdFlagBtn.translatesAutoresizingMaskIntoConstraints = false
         thirdFlagBtn.addTarget(self, action: #selector(tapOnFlag), for: .touchUpInside)
-        
+        thirdFlagBtn.contentMode = .scaleAspectFit
+        thirdFlagBtn.layer.masksToBounds = true
+        thirdFlagBtn.clipsToBounds = true
     }
-
+    
     func configureThirdFlagBtnConstraints() {
-        NSLayoutConstraint.activate([
-            thirdFlagBtn.topAnchor.constraint(equalTo: secondFlagBtn.bottomAnchor, constant: 20),
-            thirdFlagBtn.leftAnchor.constraint(equalTo: leftAnchor, constant: 100),
-            thirdFlagBtn.rightAnchor.constraint(equalTo: rightAnchor, constant: -100)
-        ])
+        thirdFlagBtn.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        thirdFlagBtn.topAnchor.constraint(greaterThanOrEqualTo: secondFlagBtn.bottomAnchor, constant: 10).isActive = true
+        widthThirdFlag.isActive = true
+        heightThirdFlag.isActive = true
     }
 }
 
@@ -139,11 +207,11 @@ extension View {
 extension View {
     func configureNextBtnLayout() {
         addSubview(nextBtn)
-
+        
         configureNextBtnSpecs()
         configureNextBtnConstraints()
     }
-
+    
     func configureNextBtnSpecs() {
         nextBtn.tag = 3
         nextBtn.translatesAutoresizingMaskIntoConstraints = false
@@ -151,16 +219,17 @@ extension View {
         nextBtn.backgroundColor = .black
         nextBtn.center = self.center
         nextBtn.contentMode = .scaleAspectFit
-
+        nextBtn.layer.masksToBounds = true
+        nextBtn.clipsToBounds = true
+        
         nextBtn.addTarget(self, action: #selector(nextChallenge), for: .touchUpInside)
     }
-
+    
     func configureNextBtnConstraints() {
-        NSLayoutConstraint.activate([
-            nextBtn.topAnchor.constraint(equalTo: nextBtn.bottomAnchor, constant: -140),
-            nextBtn.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -100),
-            nextBtn.leftAnchor.constraint(equalTo: centerXAnchor, constant: -70),
-            nextBtn.rightAnchor.constraint(equalTo: centerXAnchor, constant: 70)
-        ])
+
+        nextBtn.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        nextBtn.bottomAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
+        heightNextBtn.isActive = true
+        widthNextBtn.isActive = true
     }
 }
