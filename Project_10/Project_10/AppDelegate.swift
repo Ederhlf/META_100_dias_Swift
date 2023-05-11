7cbb3f848e6018c3cc84797d7e677ab176989100
edederhlf <ederhlf@gmail.com>
2023-05-10 23:43:56 -0300

    implement_ imagePickerController

--- Project_10/Project_10/AppDelegate.swift
+++ Project_10/Project_10/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let vc = ViewController()
+        let vc = UINavigationController(rootViewController: ViewController()) 
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
--- Project_10/Project_10/View.swift
+++ Project_10/Project_10/View.swift
@@ -2,36 +2,38 @@
 import Foundation
 import UIKit
 
+protocol PersonDelegate: AnyObject {
+    func didSelectedIndexPath(person: Person) 
+}
+
 class View: UIView {
     var collectionView: UICollectionView?
-
+    var people = [Person]()
+    weak var delegate: PersonDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
         backgroundColor = .red
         let layout = UICollectionViewFlowLayout()
-////               layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-//               layout.minimumLineSpacing = 0
-//               layout.minimumInteritemSpacing = 10
-        layout.itemSize = CGSize(width: 50, height: 100)
-//        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+//               layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.minimumLineSpacing = 3
+        layout.minimumInteritemSpacing = 3
+        layout.itemSize = CGSize(width: 50, height: 140)
         layout.scrollDirection = .horizontal
 
 
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        guard let collectionView = collectionView else {
-            return
-        }
+        guard let collectionView = collectionView else { return }
         addSubview(collectionView )
 
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(PersonCell.self, forCellWithReuseIdentifier: "cell")
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-//        (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.scrollDirection = .horizontal
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.isScrollEnabled = false
         
+        //MARK - Constraints collection
         collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
         collectionView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
@@ -41,18 +43,18 @@ class View: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
-
-
+// Delegate
 extension View: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDelegate {
     
-     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
 
      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -61,22 +63,22 @@ extension View: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, 
                 for: indexPath
         ) as? PersonCell else { return UICollectionViewCell() }
 
-        cell.imageView.image = UIImage(named: "person")
-        cell.nameLabel.text = "derder"
-//        cell.backgroundColor = .purple
+        let person = self.people[indexPath.item]
+        
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.nameLabel.text = person.name
+        cell.imageView.image = UIImage(named: path.path)
+
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         return cell
     }
     
-
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//
-//        return CGSize(width: 60 , height: 50)
-//    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-
-        return 3.0
-      }
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        delegate?.didSelectedIndexPath(person: person)
+     
+    }
 }
