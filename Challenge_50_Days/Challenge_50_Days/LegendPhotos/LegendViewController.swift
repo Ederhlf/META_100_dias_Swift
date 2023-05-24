a56faadf046c25569acc029a9aea5c6ab44dae9f
edederhlf <ederhlf@gmail.com>
2023-05-23 22:24:46 -0300

    challenge_50_created

--- Challenge_50_Days/Challenge_50_Days/LegendPhotos/LegendViewController.swift
+++ Challenge_50_Days/Challenge_50_Days/LegendPhotos/LegendViewController.swift
@@ -1,42 +1,51 @@
+//
+//  ViewController.swift
+//  Challenge_50_Days
+//
+//  Created by franklin gaspar on 23/05/23.
+//
 
 import UIKit
-import Foundation
 
-class ListNameViewController: UIViewController {
+class LegendListViewController: UIViewController {
 
-    private var myView: ListNameView?
-    private var myModel: ListNameViewModel?
+    private var myView: LegendListView?
+//    private var myModel: ListNameViewModel?
     var data = UserDefaults.standard
 
     override func loadView() {
         super.loadView()
-        myView = ListNameView()
+        myView = LegendListView()
         myView?.delegate = self
         view = myView
         
-        myModel = ListNameViewModel()
+//        myModel = ListNameViewModel()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Project_1"
+        
+        navigationController?.navigationItem.rightBarButtonItem = UIBarButtonItem
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .orange
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        myView?.names = myModel?.getImageNameList()
-    }    
+//        myView?.names = myModel?.getImageNameList()
+    }
 }
 
-extension ListNameViewController: ViewDelegate {
+extension LegendListViewController: LegendListViewDelegate {
     func selectedView(imageName: String) {
         var views = 1
          views += data.integer(forKey: imageName)
         data.set( views , forKey: imageName)
      
-        let vc = PhotoViewController(imageName: imageName, views: views)
+        let vc = PhotoDetailViewController(imageName: imageName, views: views)
         navigationController?.pushViewController(vc, animated: true)
     }
+
 }
+
